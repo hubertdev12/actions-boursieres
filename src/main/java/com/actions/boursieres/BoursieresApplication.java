94ad0d2f6b6760c2b4044be44daa9b71689b94a2
Hubert MOKET AYIWA <hubert.moketayiwa@macif.fr>
2025-02-10 21:24:10 +0100

    Initialisation du projet

---
+++ src/main/java/com/actions/boursieres/BoursieresApplication.java
@@ -0,0 +1,13 @@
+package com.actions.boursieres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoursieresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoursieresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/actions/boursieres/BoursieresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actions.boursieres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoursieresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
