9470d8f387bbc805e329d317916c6ea527075730
Hubert MOKET AYIWA <hubert.moketayiwa@macif.fr>
2025-02-11 16:22:27 +0100

    Ajout des variables d'environnement

--- src/main/java/com/actions/boursieres/controllers/PortfolioController.java
+++ src/main/java/com/actions/boursieres/controllers/PortfolioController.java
@@ -12,16 +12,31 @@ import org.springframework.web.bind.annotation.*;
 public class PortfolioController {
     private final PortfolioService portfolioService;
 
+    /**
+     * Créer un portefeuille
+     * @return
+     */
     @PostMapping
     public ResponseEntity<Portfolio> createPortfolio(){
         return ResponseEntity.ok(portfolioService.createPortfolio());
     }
 
+    /**
+     * Ajouter une action à un portefeuille
+     * @param id
+     * @param symbol
+     * @return
+     */
     @PostMapping("/{id}/stocks/{symbol}")
     public ResponseEntity<Portfolio> addStockToPortfolio(@PathVariable Long id, @PathVariable String symbol){
         return ResponseEntity.ok(portfolioService.addStockToPortfolio(id, symbol));
     }
 
+    /**
+     * Voir un portefeuille
+     * @param id
+     * @return
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Portfolio> getPortfolio(@PathVariable Long id){
         return ResponseEntity.ok(portfolioService.getPortfolio(id));
--- src/main/java/com/actions/boursieres/controllers/StockController.java
+++ src/main/java/com/actions/boursieres/controllers/StockController.java
@@ -14,16 +14,30 @@ import java.util.List;
 public class StockController {
     private final StockService stockService;
 
+    /**
+     * Ajouter une action
+     * @param stock
+     * @return
+     */
     @PostMapping
     public ResponseEntity<Stock> addStock(@RequestBody Stock stock){
         return ResponseEntity.ok(stockService.addStock(stock));
     }
 
+    /**
+     * Voir toutes les actions
+     * @return
+     */
     @GetMapping
     public ResponseEntity<List<Stock>> getAllStocks(){
         return ResponseEntity.ok(stockService.getAllStocks());
     }
 
+    /**
+     * Voir une action par symbol
+     * @param symbol
+     * @return
+     */
     @GetMapping("/{symbol}")
     public ResponseEntity<Stock> getStock(@PathVariable String symbol){
         return ResponseEntity.ok(stockService.getStockBySymbol(symbol));
